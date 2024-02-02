a39844e2e82c0d5d2b083acfe9d8fc4b96539221
Harsh Deep <deepharsh057@gmail.com>
2024-02-02 23:36:30 +0530

    log updated

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,8 +1,8 @@
 
 const Log = ({turns}) =>{
-    return (<ol>
+    return (<ol id="log">
         {turns.map((turn)=>{
-            return (<li key={`${turn.square.row} ${turn.square.col}`}>{turn.player} {turn.square.row}
+            return (<li key={`${turn.square.row} ${turn.square.col}`}>{turn.player} selected {turn.square.row}
             {turn.square.col}</li>)
         })}
     </ol>)
